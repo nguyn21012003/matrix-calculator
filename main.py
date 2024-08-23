91e4dcbed9639790e54b10e9e9163e3a32e8012c
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-08-24 01:07:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ def read_file_matrix(file):
         for row in reader:
             row_data = []
             for element in row:
-                row_data.append(int(element))
+                row_data.append((element))
             matrix.append(row_data)
         return matrix
+
 
 def print_file_matrix(file):
     matrix = []
@@ -30,9 +31,10 @@ def print_file_matrix(file):
         for row in reader:
             row_data = []
             for element in row:
-                row_data.append(int(element))
+                row_data.append(f"{element}")
             matrix.append(row_data)
         return tabulate(matrix, tablefmt="rounded_grid")
+
 
 def create_matrix(row, col):
     matrix = []
@@ -40,7 +42,7 @@ def create_matrix(row, col):
         a = []
         for j in range(col):
             print(f"Input the matrix element ROW {i+1}, COL {j+1}")
-            a.append(int(input()))
+            a.append(str(input()))
         matrix.append(a)
 
     print("The matrix is: ")
@@ -69,13 +71,10 @@ def show_file():
             file = list_files.append(file)
 
         with open("memory.csv", "w", newline="") as writefile:
-            writer = csv.DictWriter(writefile, fieldnames=[
-                "ID",
-                "Name"
-            ])
+            writer = csv.DictWriter(writefile, fieldnames=["ID", "Name"])
             writer.writeheader()
             for i in range(len(list_files)):
-                writer.writerow({'ID': f"{i+1}", 'Name': f"{list_files[i]}"})
+                writer.writerow({"ID": f"{i+1}", "Name": f"{list_files[i]}"})
 
 
 def additional(operation):
@@ -83,7 +82,8 @@ def additional(operation):
     matrix_A, matrix_B = operation.split("+")
     name_matrix_A = "matrix " + matrix_A + ".csv"
     name_matrix_B = "matrix " + matrix_B + ".csv"
-    matrix_A_pretty = print_file_matrix(name_matrix_A)# print dang ma tran nhin cho dep
+    # print dang ma tran nhin cho dep
+    matrix_A_pretty = print_file_matrix(name_matrix_A)
     matrix_B_pretty = print_file_matrix(name_matrix_B)
     rowth_A = len(read_file_matrix(name_matrix_A))
     colth_A = len(read_file_matrix(name_matrix_A)[0])
@@ -99,9 +99,17 @@ def additional(operation):
 
         for i in range(rowth_A):
             rowth_C = []
-
             for j in range(colth_A):
-                rowth_C.append( read_file_matrix(name_matrix_A)[i][j] + read_file_matrix(name_matrix_B)[i][j])
+                A_element = read_file_matrix(name_matrix_A)[i][j]
+                B_element = read_file_matrix(name_matrix_B)[i][j]
+                if not (A_element.isdigit() and B_element.isdigit()):
+                    if str(A_element) == str(B_element):
+                        len_char = len(str(A_element)) + len(str(B_element))
+                        rowth_C.append(str(len_char) + str(A_element))
+                    else:
+                        rowth_C.append(str(A_element) + "+" + str(B_element))
+                else:
+                    rowth_C.append(float(A_element) + float(B_element))
 
             new_matrix.append(rowth_C)
 
@@ -109,19 +117,31 @@ def additional(operation):
             writer = csv.writer(writefile)
             writer.writerows(new_matrix)
     else:
-        print("The number of columns in the first matrix should be equal to the number of rows in the second.")
+        print(
+            "The number of columns in the first matrix should be equal to the number of rows in the second."
+        )
+
+def det_matrix(name):
+    pass
 
 
 def matrix_calculator(operator):
-    additional(operator)
-    print(print_file_matrix("new matrix.csv"))
+    if "+" in operator:
+        additional(operator)
+        print(print_file_matrix("new matrix.csv"))
+    elif "-" in operator:
+        pass
 
 
 def promt_user(id):
     if id == 1:
         while True:
             try:
-                name = str("matrix ") + str(input("Matrix name from A to Z: ")).upper() + str(".csv")
+                name = (
+                    str("matrix ")
+                    + str(input("Matrix name from A to Z: ")).upper()
+                    + str(".csv")
+                )
                 save_matrix(name)
                 show_file()
                 print("ctrl + Z and Enter to stop")
@@ -143,6 +163,8 @@ def promt_user(id):
     elif id == 4:
         show_file()
         print(open_file_csv(str("memory.csv")))
+    elif id == 5:
+        print("Coming soon")
     else:
         return False
 
